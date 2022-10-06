b747042c366016296c46cece116504e690d5a631
chray67 <richardchavarry@yahoo.es>
2022-10-06 16:37:58 -0500

    jueves 06/10

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -171,9 +171,9 @@ function esPositivo(numero) {
   if (numero === 0) {
     return false;
   } else if (numero>0) {
-    return "es positivo";
+    return "Es positivo";
   } else {
-    return "es negativo";
+    return "Es negativo";
   }
   
 }
@@ -196,7 +196,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'hola' +' '+ nombre;
+  return 'Hola' +' '+ nombre +'!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -224,7 +224,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return euro * 1.2 + ' dolares';
+  return euro * 1.2;
 }
 
 
@@ -235,12 +235,12 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length>1) {
-    return 'dato incorrecto'
+    return 'Dato incorrecto'
   } 
   if  (letra === 'a' || letra === 'e' || letra === 'i'|| letra === 'o' || letra === 'u'){
-    return 'es vocal'
+    return 'Es vocal'
   } else {
-    return 'dato incorrecto'
+    return 'Dato incorrecto'
   }
 }
 
